304bb96a155f83666bfe45c0f796194c230dd2fe
Marcel Křesadlo <kremaca@gmail.com>
2024-03-06 21:50:46 +0100

    Remade work from seminar

---
+++ SeminarWork/Classes/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿namespace SeminarWork.Classes
+{
+    public class Rectangle
+    {
+        public int SideA { get; set; }
+        public int SideB { get; set; }
+        public int Circumference { get; set; }
+        public int Area { get; set; }
+        public double Diagonal { get; set; }
+
+        public void CalcAll()
+        {
+            CalcCircumference();
+            CalcArea();
+            CalcDiagonal();
+        }
+
+        private void CalcCircumference()
+        {
+            Circumference = 2 * (SideA + SideB);
+        }
+
+        private void CalcArea()
+        {
+            Area = SideA * SideB;
+        }
+
+        private void CalcDiagonal()
+        {
+            Diagonal = Math.Sqrt(Math.Pow(SideA, 2) + Math.Pow(SideB, 2));
+        }
+    }
+}
---
+++ SeminarWork/Classes/RectangleNew.cs
@@ -0,0 +1,16 @@
+﻿namespace SeminarWork.Classes
+
+{
+    public class RectangleNew
+    {
+        private int sideA;
+        private int sideB;
+
+        public int SideA { get => sideA; set { if (sideA != value) sideA = (value < 0) ? 0 : value; } }
+        public int SideB { get => sideB; set => sideB = Math.Abs(value); }
+        public int Circumference => 2 * (SideA + SideB);
+        public int Area => sideA * sideB;
+        public double Diagonal => Math.Sqrt(Math.Pow(SideA, 2) + Math.Pow(SideB, 2));
+
+    }
+}
