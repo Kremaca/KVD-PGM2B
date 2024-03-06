84c0fbfd4ca6269664116e6c594ef6f4dd766565
Marcel Křesadlo <kremaca@gmail.com>
2024-03-06 22:04:33 +0100

    Remade work from seminar

---
+++ SeminarWork/Classes/Record.cs
@@ -0,0 +1,25 @@
+﻿namespace SeminarWork.Classes
+{
+    public class Record
+    {
+        public DateOnly Date { get; set; }
+        private double expense;
+
+        public double Expense
+        {
+            get { return expense; }
+            set { expense = Math.Abs(value); }
+        }
+
+        private double produce;
+
+        public double Produce
+        {
+            get { return produce; }
+            set { produce = Math.Abs(value); }
+        }
+
+        public double Gain => Produce - Expense;
+
+    }
+}
