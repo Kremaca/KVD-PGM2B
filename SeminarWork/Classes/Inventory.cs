9db228babbb1b43d9840bfe4db145221cc3c98d2
markresa <markresa@KL213P08-KVD>
2024-03-21 16:22:23 +0100

    Added table and related functions

--- SeminarWork/Classes/Inventory.cs
+++ SeminarWork/Classes/Inventory.cs
@@ -1,10 +1,20 @@
-﻿namespace SeminarWork.Classes
+﻿using Microsoft.JSInterop;
+using System.Globalization;
+namespace SeminarWork.Classes
 {
     public class Inventory
     {
+        public Inventory(IJSRuntime js) 
+        {
+            this.js = js;
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("cs-CZ"); //en-US
+        }
+
+        public IJSRuntime js;
         public List<Record> Catalogue { get; set; } = new List<Record>();
         public Record Record { get; set; } = new Record();
         public string RecordsPrint { get; set; } = "";
+        public bool UpdateMode { get; set; } = false;
 
         public void AddToCatalogue() 
         {
@@ -20,6 +30,50 @@
         public void ShowAllRecords()
         {
             RecordsPrint = $"All records:<br> {string.Join("<br>", Catalogue)}";
+        }
+
+        public void ShowStatistics() 
+        {
+            RecordsPrint = "";
+            RecordsPrint += $"Minimum: {Minimum()} <br>";
+            RecordsPrint += $"Maximum: {Maximum()} <br>";
+            RecordsPrint += $"Average: {Average()} <br>";
+        }
+
+        private double Minimum() 
+        {
+            return (Catalogue.Count == 0) ? double.NaN : Catalogue.Min(x => x.Gain);
+        }
+
+        private double Maximum()
+        {
+            return (Catalogue.Count == 0) ? double.NaN : Catalogue.Max(x => x.Gain);
+        }
+
+        private double Average()
+        {
+            return (Catalogue.Count == 0) ? double.NaN : Catalogue.Average(x => x.Gain);
+        }
+
+        public async Task RecordDelete(Record record)
+        {
+            var notice = $"Do you want to delete this record? \nDate: {record.Date} - Gain: {record.Gain}";
+            if (await js.InvokeAsync<bool>("confirm", notice)) Catalogue.Remove(record);
+            //js.InvokeVoidAsync("alert", notice);
+        }
+
+        public void RecordUpdate(Record record)
+        {
+            if (Record != record)
+            {
+                UpdateMode = true;
+                Record = record;
+            }
+            else 
+            {
+                UpdateMode = false;
+                Record = new Record();
+            }
         }
     }
 }
